2071b5d0e5a5fe35a349c595e1e6ce818c4be6b1
John Heo <jungwoocity0327@gmail.com>
2025-08-06 12:40:58 +0900

    메뉴Dto 추가

---
+++ src/main/java/com/example/demo/menu/model/MenuDto.java
@@ -0,0 +1,93 @@
+package com.example.demo.menu.model;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
+
+public class MenuDto {
+    public static class Menu{
+        private Integer id;
+        private String name;
+        private Integer price;
+        private Integer calorie;
+
+        public static MenuDto.Menu from(MenuEntity entity) {
+            MenuDto.Menu dto = new MenuDto.Menu();
+            dto.setId(entity.getId());
+            dto.setName(entity.getName());
+            dto.setPrice(entity.getPrice());
+            dto.setCalorie(entity.getCalorie());
+
+            return dto;
+        }
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getPrice() {
+            return price;
+        }
+
+        public void setPrice(Integer price) {
+            this.price = price;
+        }
+
+        public Integer getCalorie() {
+            return calorie;
+        }
+
+        public void setCalorie(Integer calorie) {
+            this.calorie = calorie;
+        }
+    }
+
+    public static class Register{
+        private String name;
+        private Integer price;
+        private Integer calorie;
+
+        public MenuEntity toEntity() {
+            MenuEntity entity = new MenuEntity();
+            entity.setName(name);
+            entity.setPrice(price);
+            entity.setCalorie(calorie);
+
+            return entity;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getPrice() {
+            return price;
+        }
+
+        public void setPrice(Integer price) {
+            this.price = price;
+        }
+
+        public Integer getCalorie() {
+            return calorie;
+        }
+
+        public void setCalorie(Integer calorie) {
+            this.calorie = calorie;
+        }
+    }
+}
